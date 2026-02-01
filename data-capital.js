66b50450d0a27c9c76c0220b1664539cd3b7ac17
sajjad16j-stack <sajjad16j@gmail.com>
2026-02-01 06:40:45 +0330

    Update data-capital.js

--- data-capital.js
+++ data-capital.js
@@ -2,52 +2,52 @@ TAB_DATA.capital={
    items: [
 
     {
-      title: "Laptop",
+      title: "Building Deposit",
       color: "#1e40af",
-      unitCost: 1200,
+      unitCost: 12500,
       countByMonth: [
-        5,5,6,6,6,7,7,8,8,8,9,9,
-        10,10,10,10,11,11,11,12,12,12,12,12
+        1,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0
       ]
     },
 
     {
-      title: "Monitor",
+      title: "Office Decoration",
       color: "#3b82f6",
-      unitCost: 300,
+      unitCost: 12500,
       countByMonth: [
-        10,10,12,12,12,14,14,16,16,16,18,18,
-        20,20,22,22,22,24,24,26,26,26,28,28
+        1,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0
       ]
     },
 
     {
-      title: "Printer",
+      title: "Supplies & Equipment",
       color: "#f59e0b",
-      unitCost: 500,
+      unitCost: 54000,
       countByMonth: [
-        2,2,2,3,3,3,3,4,4,4,5,5,
-        5,5,6,6,6,6,7,7,7,8,8,8
+      1,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0
       ]
     },
 
     {
-      title: "Office Chair",
+      title: "Server",
       color: "#10b981",
-      unitCost: 150,
+      unitCost: 19000,
       countByMonth: [
-        20,20,20,21,21,21,22,22,23,23,23,24,
-        24,25,25,25,26,26,27,27,28,28,28,29
+         1,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0
       ]
     },
 
     {
-      title: "Projector",
+      title: "Computers",
       color: "#ec4899",
-      unitCost: 800,
+      unitCost: 65000,
       countByMonth: [
-        1,1,1,1,2,2,2,2,3,3,3,3,
-        3,3,3,3,4,4,4,4,5,5,5,5
+        1,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0
       ]
     }
 ]
